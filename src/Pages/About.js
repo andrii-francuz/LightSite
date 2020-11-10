bcee4b28935ed4740b4379a9f8c4fabbc7670095
andrii-francuz <andrii.fryntsko@gmail.com>
2020-11-10 22:59:01 +0200

    added: Tab menu on About page; deleted: Text on About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { Container, Nav, Tab, Row, Col } from 'react-bootstrap'
 
 export default class About extends Component {
     render() {
         return (
-            <div>
-                About
-            </div>
+            <Container>
+                <Tab.Container id="right-tabs-example" defaultActiveKey="first">
+                    <Row>
+                        <Col md="3">
+                            <Nav variant="pills" className="flex-column mt-2">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">First</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Second</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Third</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="fourth">Fourth</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col md="9">
+                            
+                        </Col>
+                    </Row>
+                </Tab.Container>
+            </Container>
         )
     }
 }
